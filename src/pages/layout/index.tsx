1654170bf2223d47d1fd81600d9552d7fc2d539a
Jankin Sun <jingshuang.sun@goodwe.com>
2023-02-15 18:08:46 +0800

    Add navbar to blog page

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import Background from "./components/background";
 import styles from "./index.module.less";
 import { animated, useSpring } from "@react-spring/web";
+import DocsPage from "./docs";
 
 const IndexPage = () => {
   const [active, setActive] = useState<boolean>(false);
+  const [curPage, setCurPage] = useState<string>("index");
 
   const contentStyles = useSpring({
     width: active ? "100%" : "50%",
@@ -13,26 +15,50 @@ const IndexPage = () => {
     width: active ? "0%" : "50%",
   });
 
+  const renderContent = () => {
+    if (curPage === "index") return <div>index</div>;
+    if (curPage === "docs") return <DocsPage />;
+    return <div>ERROR</div>;
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <div className={styles.logo}>
-          <span>Jankin </span>
+          <span
+            onClick={() => {
+              setActive(false);
+              setCurPage("index");
+            }}
+          >
+            <span>Jankin </span>
 
-          <span>Sun</span>
+            <span>Sun</span>
+          </span>
         </div>
         <div className={styles.navMenu}>
-          <div className={styles.menuItem} onClick={() => setActive(true)}>
-            Blog
+          {/* <div
+            className={styles.menuItem}
+            onClick={() => {
+              setActive(true);
+              setCurPage("index");
+            }}
+          >
+            Jankin
+          </div> */}
+          <div
+            className={styles.menuItem}
+            onClick={() => {
+              setActive(true);
+              setCurPage("docs");
+            }}
+          >
+            Docs
           </div>
         </div>
       </div>
-      <animated.div
-        className={styles.content}
-        style={contentStyles}
-        // onClick={() => setActive(!active)}
-      >
-        <div>123</div>
+      <animated.div className={styles.content} style={contentStyles}>
+        {renderContent()}
       </animated.div>
       <animated.div className={styles.background} style={bgStyles}>
         <Background />
