6b4920d28581b28852c7578d8cf83ed0e63b7725
Jankin Sun <jingshuang.sun@goodwe.com>
2023-03-09 19:00:17 +0800

    add content page

---
+++ src/pages/layout/home/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./index.module.less";
+
+const HomePage = () => {
+  return (
+    <div className={styles.container}>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default HomePage;
